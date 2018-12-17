b731f18c7d5983ea9f55a00f5e7d69e45a0ebcb6
Mohammad-Ali A'RÂBI <angellandros@gmail.com>
2018-12-17 21:07:15 +0100

    Add basic Spring Boot project

---
+++ src/main/java/com/aerabi/mancala/MancalaApplication.java
@@ -0,0 +1,14 @@
+package com.aerabi.mancala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancalaApplication.class, args);
+	}
+
+}
+
